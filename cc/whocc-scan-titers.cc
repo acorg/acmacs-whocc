c85ca5790bb9e6f04b427ac8698ca30e9246dde0
Eugene Skepner <github@skepner.eu>
2017-05-04 16:26:27 +0200

    whocc-scan-titers development

---
+++ cc/whocc-scan-titers.cc
@@ -0,0 +1,103 @@
+#include <string>
+
+#pragma GCC diagnostic push
+#include "acmacs-base/boost-diagnostics.hh"
+#include "boost/program_options.hpp"
+#include <boost/filesystem.hpp>
+#pragma GCC diagnostic pop
+
+#include "acmacs-chart/ace.hh"
+
+namespace fs = boost::filesystem;
+
+// ----------------------------------------------------------------------
+
+void scan(const fs::path& source_dir, std::vector<fs::path>& ace_files);
+
+// ----------------------------------------------------------------------
+
+class Options
+{
+ public:
+    std::string source_dir;
+};
+
+static int get_args(int argc, const char *argv[], Options& aOptions);
+
+int main(int argc, const char *argv[])
+{
+    Options options;
+    int exit_code = get_args(argc, argv, options);
+    if (exit_code == 0) {
+        try {
+            std::vector<fs::path> ace_files;
+            scan(fs::path(options.source_dir), ace_files);
+            std::cout << ace_files.size() << std::endl;
+            for (const auto& p: ace_files)
+                std::cout << p << std::endl;
+        }
+        catch (std::exception& err) {
+            std::cerr << err.what() << std::endl;
+            exit_code = 1;
+        }
+    }
+    return exit_code;
+}
+
+static int get_args(int argc, const char *argv[], Options& aOptions)
+{
+    using namespace boost::program_options;
+    options_description desc("Options");
+    desc.add_options()
+            ("help", "Print help messages")
+            ("source,s", value<std::string>(&aOptions.source_dir)->required(), "directory to scan for ace files (recursively)")
+            ;
+    positional_options_description pos_opt;
+    pos_opt.add("source", 1);
+
+    variables_map vm;
+    try {
+        store(command_line_parser(argc, argv).options(desc).positional(pos_opt).run(), vm);
+        if (vm.count("help")) {
+            std::cerr << desc << std::endl;
+            return 1;
+        }
+        notify(vm);
+        return 0;
+    }
+    catch(required_option& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cerr << desc << std::endl;
+          // std::cerr << "Usage: " << argv[0] << " <tree.json> <output.pdf>" << std::endl;
+        return 2;
+    }
+    catch(error& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cerr << desc << std::endl;
+        return 3;
+    }
+
+} // get_args
+
+// ----------------------------------------------------------------------
+
+void scan(const fs::path& source_dir, std::vector<fs::path>& ace_files)
+{
+    if (!fs::is_directory(source_dir))
+        throw std::runtime_error(source_dir.string() + " is not a directory");
+    for (fs::directory_entry& dirent: fs::directory_iterator(source_dir)) {
+        if (fs::is_directory(dirent.status()))
+            scan(dirent.path(), ace_files);
+        else if (is_regular_file(dirent.status()) && dirent.path().extension().string() == ".ace")
+            ace_files.push_back(dirent.path());
+    }
+
+} // scan
+
+// ----------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
