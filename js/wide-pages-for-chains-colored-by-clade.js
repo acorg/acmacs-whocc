1c4793b264565fa37765da551ae38a702d47f5fd
Eugene Skepner <github@skepner.eu>
2018-04-17 16:55:57 +0200

    wide-pages-for-chains-colored-by-clade via acmacs::Surface::JsDynamic

--- js/wide-pages-for-chains-colored-by-clade.js
+++ js/wide-pages-for-chains-colored-by-clade.js
@@ -5,22 +5,30 @@ export function main(wide_page_data) {
     const start = new Date();
 
     $("body").append("<table><tr class='column_names'></tr><tr class='head_individual'></tr><tr class='individual'></tr><tr class='head_incremental'></tr><tr class='incremental'></tr><tr class='head_scratch'></tr><tr class='scratch'></tr></table>");
-    wide_page_data.forEach(entry => {
+    wide_page_data.maps.forEach(entry => {
         $("tr.column_names").append("<td>" + entry["1"].name + "</td");
-        $("tr.head_individual").append("<td>" + (entry["1"] ? entry["1"].stress.toFixed(2) : "") + "</td");
-        if (entry["1"]) {
-            $("tr.individual").append("<td><canvas id='" + entry["1"].chart_id + "' /></td>");
-            draw_on(entry["1"].chart_id);
-        }
-        else {
-            $("tr.individual").append("<td></td>");
-        }
-        $("tr.head_incremental").append("<td>" + (entry.i ? entry.i.stress.toFixed(2) : "") + "</td");
-        $("tr.head_scratch").append("<td>" + (entry.s ? entry.s.stress.toFixed(2) : "") + "</td");
+        add_column(entry["1"], "individual");
+        add_column(entry.i, "incremental");
+        add_column(entry.s, "scratch");
     });
-
+    $("body").append("<p class='chain_id'>Chain: " + wide_page_data.chain_id + "</p>");
+    $("body").append("<p class='modified'>" + wide_page_data.generated + "</p>");
     const elapsed = new Date() - start;
     console.log("wide page drawing time: " + elapsed + "ms -> " + (1000 / elapsed).toFixed(1) + "fps");
+}
+
+// ----------------------------------------------------------------------
+
+function add_column(entry, tag) {
+    if (entry) {
+        $("tr.head_" + tag).append("<td>" + entry.stress.toFixed(2) + "</td");
+        $("tr." + tag).append("<td><canvas id='" + entry.chart_id + "' /></td>");
+        draw_on(entry.chart_id);
+    }
+    else {
+        $("tr.head_" + tag).append("<td></td>");
+        $("tr." + tag).append("<td></td>");
+    }
 }
 
 function draw_on(chart_id) {
@@ -29,3 +37,8 @@ function draw_on(chart_id) {
         draw($("#" + chart_id), [map_data, settings]);
     });
 }
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
