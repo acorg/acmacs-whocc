e67b9f9da799a6e7410f0bd9ef2b6a478dcc3113
Eugene Skepner <github@skepner.eu>
2018-09-07 13:39:00 +0200

    whocc-chart-hidb-seqdb-report

--- cc/whocc-chart-hidb-seqdb-report.cc
+++ cc/whocc-chart-hidb-seqdb-report.cc
@@ -2,12 +2,36 @@
 
 #include "acmacs-base/argc-argv.hh"
 #include "acmacs-base/string.hh"
+#include "acmacs-base/range.hh"
 #include "acmacs-base/stream.hh"
 #include "acmacs-chart-2/factory-import.hh"
 #include "acmacs-chart-2/chart.hh"
 #include "seqdb/seqdb.hh"
 #include "hidb-5/hidb.hh"
 #include "hidb-5/report.hh"
+
+// ----------------------------------------------------------------------
+
+struct AntigenData
+{
+    size_t no;
+    const hidb::HiDb* hidb = nullptr;
+    std::shared_ptr<acmacs::chart::Antigen> antigen_chart;
+    std::shared_ptr<hidb::Antigen> antigen_hidb;
+    seqdb::SeqdbEntrySeq antigen_seqdb;
+};
+
+inline std::ostream& operator<<(std::ostream& out, const AntigenData& ag)
+{
+    out << ag.no << ' ' << *ag.antigen_chart;
+    if (ag.antigen_hidb)
+        hidb::report_tables(out, *ag.hidb, ag.antigen_hidb->tables(), hidb::report_tables::recent, "\n    ");
+    if (ag.antigen_seqdb) {
+        if (const auto& clades = ag.antigen_seqdb.seq().clades(); !clades.empty())
+            out << "\n    clades: " << clades;
+    }
+    return out;
+}
 
 // ----------------------------------------------------------------------
 
@@ -34,21 +58,17 @@ int main(int argc, char* const argv[])
             const auto virus_type = chart->info()->virus_type(acmacs::chart::Info::Compute::Yes);
             const auto& seqdb = seqdb::get();
             const auto& hidb = hidb::get(virus_type, verbose ? report_time::Yes : report_time::No);
-            auto antigens = chart->antigens();
-            const auto hidb_antigens = hidb.antigens()->find(*antigens);
-            const auto seqdb_antigens = seqdb.match(*antigens, virus_type);
-            for (size_t ag_no = 0; ag_no < antigens->size(); ++ag_no) {
-                std::cout
-                          // << std::setw(5) << std::right
-                        << ag_no << ' ' << antigens->at(ag_no)->full_name() << '\n';
-                if (const auto& hidb_entry = hidb_antigens[ag_no]; hidb_entry) {
-                    hidb::report_antigen(hidb, *hidb_entry, true);
-                }
-                if (const auto& entry_seq = seqdb_antigens[ag_no]; entry_seq) {
-                    if (const auto& clades = entry_seq.seq().clades(); !clades.empty())
-                        std::cout << "    clades: " << clades << '\n';
-                }
-            }
+            auto antigens_chart = chart->antigens();
+            const auto hidb_antigens = hidb.antigens()->find(*antigens_chart);
+            const auto seqdb_antigens = seqdb.match(*antigens_chart, virus_type);
+            std::vector<AntigenData> antigens(antigens_chart->size());
+            std::transform(acmacs::index_iterator(0UL), acmacs::index_iterator(antigens_chart->size()), antigens.begin(), [&](size_t index) -> AntigenData {
+                return {index, &hidb, antigens_chart->at(index), hidb_antigens[index], seqdb_antigens[index]};
+            });
+
+            for (const auto& ad : antigens)
+                std::cout << ad << '\n';
+
         }
     }
     catch (std::exception& err) {
