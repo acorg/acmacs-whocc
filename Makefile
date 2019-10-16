194bc0a01971674e2000906279f479ece1644394
Eugene Skepner <github@skepner.eu>
2019-10-16 10:42:50 +0200

    using seqdb-3

--- Makefile
+++ Makefile
@@ -24,7 +24,7 @@ LDLIBS = \
   $(AD_LIB)/$(call shared_lib_name,libacmacsvirus,1,0) \
   $(AD_LIB)/$(call shared_lib_name,libacmacschart,2,0) \
   $(AD_LIB)/$(call shared_lib_name,libhidb,5,0) \
-  $(AD_LIB)/$(call shared_lib_name,libseqdb,2,0) \
+  $(AD_LIB)/$(call shared_lib_name,libseqdb,3,0) \
   $(AD_LIB)/$(call shared_lib_name,libacmacsdraw,1,0) \
   $(CAIRO_LIBS) $(XZ_LIBS) $(CXX_LIBS)
 
--- cc/whocc-chart-hidb-seqdb-report.cc
+++ cc/whocc-chart-hidb-seqdb-report.cc
@@ -1,13 +1,13 @@
 #include <iostream>
 
-#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/argv.hh"
 #include "acmacs-base/string.hh"
 #include "acmacs-base/string-split.hh"
 #include "acmacs-base/range.hh"
 #include "acmacs-base/stream.hh"
 #include "acmacs-chart-2/factory-import.hh"
 #include "acmacs-chart-2/chart.hh"
-#include "seqdb/seqdb.hh"
+#include "seqdb-3/seqdb.hh"
 #include "hidb-5/hidb.hh"
 #include "hidb-5/report.hh"
 
@@ -19,7 +19,7 @@ struct AntigenData
     const hidb::HiDb* hidb = nullptr;
     std::shared_ptr<acmacs::chart::Antigen> antigen_chart;
     std::shared_ptr<hidb::Antigen> antigen_hidb;
-    seqdb::SeqdbEntrySeq antigen_seqdb;
+    acmacs::seqdb::ref antigen_seqdb;
 };
 
 inline std::ostream& operator<<(std::ostream& out, const AntigenData& ag)
@@ -28,7 +28,7 @@ inline std::ostream& operator<<(std::ostream& out, const AntigenData& ag)
     if (ag.antigen_hidb)
         hidb::report_tables(out, *ag.hidb, ag.antigen_hidb->tables(), hidb::report_tables::recent, "\n    ");
     if (ag.antigen_seqdb) {
-        if (const auto& clades = ag.antigen_seqdb.seq().clades(); !clades.empty())
+        if (const auto& clades = ag.antigen_seqdb.seq().clades; !clades.empty())
             out << "\n    clades: " << clades;
     }
     return out;
@@ -36,77 +36,73 @@ inline std::ostream& operator<<(std::ostream& out, const AntigenData& ag)
 
 // ----------------------------------------------------------------------
 
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    // option<str> db_dir{*this, "db-dir"};
+    option<str> seqdb{*this, "seqdb"};
+
+    option<bool>      sort_by_tables{*this, "sort-by-tables"};
+    option<str>       clade{*this, "clade"};
+    option<str>       aa{*this, "aa", desc{"report AA at given positions (comma separated)"}};
+    option<bool>      verbose{*this, 'v', "verbose"};
+
+    argument<str> chart_file{*this, arg_name{"chart"}, mandatory};
+};
+
 int main(int argc, char* const argv[])
 {
     int exit_code = 0;
     try {
-        argc_argv args(argc, argv,
-                       {
-                           {"--sort-by-tables", false},
-                           {"--clade", ""},
-                           {"--aa", "", "report AA at given positions (comma separated)"},
-                           {"--db-dir", ""},
-                           {"-h", false},
-                           {"--help", false},
-                           {"-v", false},
-                           {"--verbose", false},
-                       });
-        if (args["-h"] || args["--help"] || args.number_of_arguments() != 1) {
-            std::cerr << "Usage: " << args.program() << " [options] <chart-file>\n" << args.usage_options() << '\n';
-            exit_code = 1;
-        }
-        else {
-            const bool verbose = args["-v"] || args["--verbose"];
-            const bool sort_by_tables = args["--sort-by-tables"];
-            const std::string clade(args["--clade"]);
-            const std::string aa_opt(args["--aa"]);
-            const std::vector<size_t> report_aa_at_pos = aa_opt.empty() ? std::vector<size_t>{} : acmacs::string::split_into_size_t(static_cast<std::string>(args["--aa"]), ",");
+        Options opt(argc, argv);
+        acmacs::seqdb::setup(opt.seqdb);
+        const std::vector<size_t> report_aa_at_pos = opt.aa ? acmacs::string::split_into_size_t(*opt.aa, ",") : std::vector<size_t>{};
 
-            seqdb::setup_dbs(std::string(args["--db-dir"]), verbose ? seqdb::report::yes : seqdb::report::no);
-            auto chart = acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report_time::no);
-            const auto virus_type = chart->info()->virus_type(acmacs::chart::Info::Compute::Yes);
-            const auto& seqdb = seqdb::get();
-            const auto& hidb = hidb::get(virus_type, do_report_time(verbose));
-            auto antigens_chart = chart->antigens();
-            const auto hidb_antigens = hidb.antigens()->find(*antigens_chart);
-            const auto seqdb_antigens = seqdb.match(*antigens_chart, virus_type);
-            std::vector<AntigenData> antigens(antigens_chart->size());
-            std::transform(acmacs::index_iterator(0UL), acmacs::index_iterator(antigens_chart->size()), antigens.begin(), [&](size_t index) -> AntigenData {
-                return {index, &hidb, antigens_chart->at(index), hidb_antigens[index], seqdb_antigens[index]};
-            });
-            if (!clade.empty()) {
-                antigens.erase(std::remove_if(antigens.begin(), antigens.end(), [&](const auto& ag) -> bool { return !ag.antigen_seqdb || !ag.antigen_seqdb.seq().has_clade(clade); }), antigens.end());
-            }
-            std::cerr << "INFO: " << antigens.size() << " antigens upon filtering\n";
-            if (sort_by_tables) {
-                auto hidb_tables = hidb.tables();
-                std::sort(antigens.begin(), antigens.end(), [&](const auto& e1, const auto& e2) -> bool {
-                    if (!e1.antigen_hidb)
-                        return false;
-                    if (!e2.antigen_hidb)
-                        return true;
-                    if (const auto nt1 = e1.antigen_hidb->number_of_tables(), nt2 = e2.antigen_hidb->number_of_tables(); nt1 == nt2) {
-                        auto mrt1 = hidb_tables->most_recent(e1.antigen_hidb->tables()), mrt2 = hidb_tables->most_recent(e2.antigen_hidb->tables());
-                        return mrt1->date() > mrt2->date();
-                    }
-                    else
-                        return nt1 > nt2;
-                });
-            }
-            for (const auto& ad : antigens) {
-                std::cout << ad << '\n';
-                if (!report_aa_at_pos.empty()) {
-                    const auto aa = ad.antigen_seqdb.seq().amino_acids(true);
-                    std::cout << "   ";
-                    for (auto pos : report_aa_at_pos) {
-                        std::cout << ' ' << pos;
-                        if (aa.size() >= pos)
-                            std::cout << aa[pos - 1];
-                        else
-                            std::cout << '?';
-                    }
-                    std::cout << '\n';
+        auto chart = acmacs::chart::import_from_file(opt.chart_file);
+        const auto virus_type = chart->info()->virus_type(acmacs::chart::Info::Compute::Yes);
+        const auto& seqdb = acmacs::seqdb::get();
+        const auto& hidb = hidb::get(virus_type); // , do_report_time(verbose));
+        auto antigens_chart = chart->antigens();
+        const auto hidb_antigens = hidb.antigens()->find(*antigens_chart);
+        const auto seqdb_antigens = seqdb.match(*antigens_chart, virus_type);
+        std::vector<AntigenData> antigens(antigens_chart->size());
+        std::transform(acmacs::index_iterator(0UL), acmacs::index_iterator(antigens_chart->size()), antigens.begin(), [&](size_t index) -> AntigenData {
+            return {index, &hidb, antigens_chart->at(index), hidb_antigens[index], seqdb_antigens[index]};
+        });
+        if (opt.clade)
+            antigens.erase(std::remove_if(antigens.begin(), antigens.end(), [clade = *opt.clade](const auto& ag) -> bool { return !ag.antigen_seqdb || !ag.antigen_seqdb.seq().has_clade(clade); }),
+                           antigens.end());
+        std::cerr << "INFO: " << antigens.size() << " antigens upon filtering\n";
+        if (opt.sort_by_tables) {
+            auto hidb_tables = hidb.tables();
+            std::sort(antigens.begin(), antigens.end(), [&](const auto& e1, const auto& e2) -> bool {
+                if (!e1.antigen_hidb)
+                    return false;
+                if (!e2.antigen_hidb)
+                    return true;
+                if (const auto nt1 = e1.antigen_hidb->number_of_tables(), nt2 = e2.antigen_hidb->number_of_tables(); nt1 == nt2) {
+                    auto mrt1 = hidb_tables->most_recent(e1.antigen_hidb->tables()), mrt2 = hidb_tables->most_recent(e2.antigen_hidb->tables());
+                    return mrt1->date() > mrt2->date();
                 }
+                else
+                    return nt1 > nt2;
+            });
+        }
+        for (const auto& ad : antigens) {
+            std::cout << ad << '\n';
+            if (!report_aa_at_pos.empty()) {
+                const auto aa = ad.antigen_seqdb.seq().amino_acids.aligned();
+                std::cout << "   ";
+                for (auto pos : report_aa_at_pos) {
+                    std::cout << ' ' << pos;
+                    if (aa.size() >= pos)
+                        std::cout << aa[pos - 1];
+                    else
+                        std::cout << '?';
+                }
+                std::cout << '\n';
             }
         }
     }
