e13145d61de13a3ac4e06f91ea1d102ac95cfee7
Eugene Skepner <github@skepner.eu>
2020-12-06 12:45:26 +0100

    python interface for importing tables

--- cc/data-fix-py.cc
+++ cc/data-fix-py.cc
@@ -9,8 +9,11 @@
 #pragma GCC diagnostic ignored "-Wmissing-variable-declarations" // 2.6.1 2020-12-06
 #endif
 
-PYBIND11_EMBEDDED_MODULE(data_fix_module, mdl) {
-    mdl.attr("a") = 77;
+PYBIND11_EMBEDDED_MODULE(data_fix_module, mdl)
+{
+    mdl.def("name_antigen_serum_fix", [](std::string_view rex, std::string_view repl) { AD_DEBUG("name_antigen_serum_fix \"{}\" \"{}\"", rex, repl); });
+    mdl.def("passage_antigen_serum_fix", [](std::string_view rex, std::string_view repl) { AD_DEBUG("passage_antigen_serum_fix \"{}\" \"{}\"", rex, repl); });
+    mdl.def("titer_fix", [](std::string_view rex, std::string_view repl) { AD_DEBUG("titer_fix \"{}\" \"{}\"", rex, repl); });
 }
 
 #pragma GCC diagnostic pop
@@ -19,15 +22,10 @@ PYBIND11_EMBEDDED_MODULE(data_fix_module, mdl) {
 
 void acmacs::data_fix::v1::py_init(const std::vector<std::string_view>& scripts)
 {
-    AD_DEBUG("py_init");
-    py::exec(R"(
-import sys
-from data_fix_module import *
-print(f"data_fix a={a}", file=sys.stderr)
-)",
-             py::globals()); // , locals);
-
-    AD_DEBUG("py_init");
+    for (const auto& script : scripts) {
+        AD_DEBUG("import {}", script);
+        py::eval_file(pybind11::str{script.data(), script.size()});
+    }
 
 } // acmacs::data_fix::v1::py_init
 
