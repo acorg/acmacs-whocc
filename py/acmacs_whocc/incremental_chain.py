94123bdc3f5fb3664860dd6002cc8405fd1a1b87
Eugene Skepner <github@skepner.eu>
2020-12-26 17:05:01 +0100

    incremental chain

--- py/acmacs_whocc/incremental_chain.py
+++ py/acmacs_whocc/incremental_chain.py
@@ -1,4 +1,4 @@
-import sys, os, re, json, datetime, time
+import sys, os, re, json, datetime, time, subprocess
 from pathlib import Path
 import logging; module_logger = logging.getLogger(__name__)
 
@@ -6,6 +6,13 @@ sys.path[:0] = [str(Path(os.environ["ACMACSD_ROOT"]).resolve().joinpath("lib"))]
 import acmacs
 
 # ======================================================================
+
+def detect_processor():
+    try:
+        subprocess.check_output(["condor_version"])
+        return "htcondor"
+    except (subprocess.SubprocessError, FileNotFoundError):
+        return "built-in"
 
 sDefaultParameters = {
     "number_of_optimizations": 100,
@@ -21,7 +28,13 @@ sDefaultParameters = {
 
     "state_filename": Path("state.json"),
     "output_dir": Path("out"),
+    "htcondor_dir": Path("htcondor"),
+    "email": "eu@antigenic-cartography.org",
+    "threads": 16,
+    "number_of_optimizations_per_run": 100,
     "sleep_interval_when_not_ready": 10, # in seconds
+
+    "processor": detect_processor(),
 }
 
 class Error (RuntimeError): pass
@@ -40,7 +53,7 @@ def main(source_tables, param):
 # ----------------------------------------------------------------------
 
 def chain(source_tables, param):
-    state = State(source_tables=source_tables, param=param, processor=ProcessorBuiltIn())
+    state = State(source_tables=source_tables, param=param, processor=processor_factory(param["processor"]))
     while state.has_todo():
         state.check_running()
         if not state.run_ready():
@@ -89,12 +102,7 @@ class Step:
         if not self.out:
             module_logger.warning(f"step {self.step_id()} has not output, it is always completed")
             return True
-        try:
-            # now = time.time()
-            # return all((cPath(out).stat().st_mtime - now) > 2 for out in self.out) # out was modified more than 2 seconds ago
-            return all(chain_state.processor.output_ready(Path(out)) for out in self.out)
-        except FileNotFoundError:
-            return False
+        return all(chain_state.processor.output_ready(Path(out)) for out in self.out)
 
     def is_failed(self):
         return False
@@ -204,6 +212,9 @@ class State:
         self.state = {"setup": {"number_of_optimizations": 0}, "steps": {}}
         self.state_file = Path(param["state_filename"])
         self.output_dir = Path(param["output_dir"])
+        self.htcondor_dir = Path(param["htcondor_dir"])
+        self.email = param["email"]
+        self.threads = param["threads"]
         self.load(source_tables, param)
         self.processor = processor
 
@@ -274,7 +285,7 @@ class State:
 
     def update(self, source_tables, param):
         updated = False
-        for changeable_value in ["number_of_optimizations", "projections_to_keep"]:
+        for changeable_value in ["number_of_optimizations", "projections_to_keep", "number_of_optimizations_per_run"]:
             updated = self.update_value(changeable_value, param[changeable_value], raise_if_changed=False)
         for readonly_value in ["number_of_dimensions", "minimum_column_basis"]:
             updated = self.update_value(readonly_value, param[readonly_value], raise_if_changed=True) or updated
@@ -340,6 +351,16 @@ class ProcessorTimer:
         self.step.runtime = str(self.step.finish - self.step.start)
         module_logger.info(f"{self.step.step_id()} {self.step.type_desc()} {self.chart.make_name()}\n{'':30s}<{self.step.runtime}>")
 
+def processor_factory(processor_type):
+    if processor_type == "built-in":
+        return ProcessorBuiltIn()
+    elif processor_type == "htcondor":
+        return ProcessorHTCondor()
+    else:
+        raise Error(f"""unknown processor type {processor_type!r}""")
+
+# ----------------------------------------------------------------------
+
 class Processor:
 
     def merge_incremental(self, chain_state, step):
@@ -351,6 +372,8 @@ class Processor:
     def export(self, chart, out :Path):
         out.parent.mkdir(parents=True, exist_ok=True)
         chart.export(str(out), sys.argv[0])
+
+# ----------------------------------------------------------------------
 
 class ProcessorBuiltIn (Processor):
 
@@ -375,10 +398,54 @@ class ProcessorBuiltIn (Processor):
             step.stress = chart.projection().stress()
             pt.chart = chart
 
+# ----------------------------------------------------------------------
+
 class ProcessorHTCondor (Processor):
 
-    def output_ready(self, out :Path):
-        return (out.stat().st_mtime - time.time()) > 2 # out was modified more than 2 seconds ago
+    def output_ready(self, out :Path = None):
+        try:
+            return (out.stat().st_mtime - time.time()) > 2 # out was modified more than 2 seconds ago
+        except FileNotFoundError:
+            return False
+
+    def processing_dir(self, chain_state, step):
+        pd = chain_state.htcondor_dir.joinpath(f"{step.step_id()}.{datetime.datetime.now().strftime('%Y-%m%d-%H%M%S')}")
+        pd.mkdir(parents=True, exist_ok=False)
+        return pd
+
+    def relax_from_scratch(self, chain_state, step):
+        from acmacs_base import htcondor
+        step.processing_dir = self.processing_dir(chain_state, step)
+        number_of_optimizations = chain_state.setup()["number_of_optimizations_per_run"]
+        queue_size = int(chain_state.setup()["number_of_optimizations"] / number_of_optimizations) + (1 if (chain_state.setup()["number_of_optimizations"] % number_of_optimizations) > 0 else 0)
+        common_args = [
+            "-d", chain_state.setup()["number_of_dimensions"],
+            "-n", number_of_optimizations,
+            "-m", chain_state.setup()["minimum_column_basis"],
+            "--keep-projections", chain_state.setup()["projections_to_keep"],
+            "--remove-original-projections",
+            "--threads", chain_state.threads,
+        ]
+        program_args = [[common_args + [str(Path(step.src[0]).resolve()), f"{run_no:04d}.ace"]] for run_no in range(queue_size)]
+        desc_filename, step.condor_log = htcondor.prepare_submission(
+            program=Path(os.environ["ACMACSD_ROOT"], "bin", "chart-relax"),
+            program_args=program_args,
+            description=f"chart-relax {step.step_id()}",
+            current_dir=step.processing_dir, capture_stdout=True, email=chain_state.email, notification="Error", request_cpus=chain_state.threads)
+        step.cluster = htcondor.submit(desc_filename)
+
+    def relax_incremental(self, chain_state, step):
+        from acmacs_base import htcondor
+        processing_dir = self.processing_dir(chain_state, step)
+        # with ProcessorTimer(step) as pt:
+        #     chart = acmacs.Chart(str(step.src[0]))
+        #     chart.relax_incremental(number_of_optimizations=chain_state.setup()["number_of_optimizations"])
+        #     chart.keep_projections(chain_state.setup()["projections_to_keep"])
+        #     self.export(chart=chart, out=Path(step.out[0]))
+        #     step.stress = chart.projection().stress()
+        #     pt.chart = chart
+
+# ----------------------------------------------------------------------
 
 # ======================================================================
 
