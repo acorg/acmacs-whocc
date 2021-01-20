e178564c12d446e87392221da19b6ee0243849bb
Eugene Skepner <github@skepner.eu>
2021-01-20 17:04:34 +0100

    Crick serum_id handling

--- cc/sheet-extractor.cc
+++ cc/sheet-extractor.cc
@@ -39,7 +39,7 @@ static const std::regex re_CDC_antigen_control{R"(\bCONTROL\b)", acmacs::regex::
 
 static const std::regex re_CRICK_serum_name_1{"^([AB]/[A-Z '_-]+|NYMC\\s+X-[0-9]+[A-Z]*)$", acmacs::regex::icase};
 static const std::regex re_CRICK_serum_name_2{"^[A-Z0-9-/]+$", acmacs::regex::icase};
-static const std::regex re_CRICK_serum_id{R"(^(?:[A-Z\s]+\s+)?\s*(F[0-9][0-9]/[0-2][0-9])(?:\*(\d))?$)", acmacs::regex::icase};
+static const std::regex re_CRICK_serum_id{R"(^(?:[A-Z\s]+\s+)?\s*(F[0-9][0-9]/[0-2][0-9]|SH[\s\d,]+)(?:\*(\d)(?:,\d)?)?$)", acmacs::regex::icase};
 static const std::regex re_CRICK_less_than{R"(^\s*<\s*=\s*(<\d+)\s*$)", acmacs::regex::icase};
 static const std::regex re_CRICK_less_than_multi{R"(^\s*\d\s*<\s*=\s*<\d+\s*;)", acmacs::regex::icase};
 static const std::regex re_CRICK_less_than_multi_entry{R"(^\s*(\d)\s*<\s*=\s*(<\d+)\s*$)", acmacs::regex::icase};
@@ -945,6 +945,9 @@ acmacs::sheet::v1::serum_fields_t acmacs::sheet::v1::ExtractorCrick::serum(size_
     }
     else
         serum.name = "*no serum_name_[12]_row_*";
+
+    if (std::smatch match; std::regex_match(serum.serum_id, match, re_CRICK_serum_id))
+        serum.serum_id = match.str(1);
     return serum;
 
 } // acmacs::sheet::v1::ExtractorCrick::serum
