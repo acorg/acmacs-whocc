5f5ce14a38094860c69d0e0bbf82b470f4941072
Eugene Skepner <github@skepner.eu>
2021-07-14 15:50:06 +0200

    chains-202105 web

--- web/chains-202105/py/directories.py
+++ web/chains-202105/py/directories.py
@@ -8,7 +8,7 @@ class CladeData:
     sSubtypeToCladePrefix = {"h1pdm": "clades-A(H1N1)PDM09", "h3": "clades-A(H3N2)", "bvic": "clades-BVICTORIA", "byam": "clades-BYAMAGATA"}
 
     def __init__(self):
-        self.data = json.load(Path("clades.json").open())
+        self.data = json.load(Path("clades.mapi").open())
 
     def entry_names_for_subtype(self, subtype):
         subtype_prefix = self.sSubtypeToCladePrefix[subtype]
