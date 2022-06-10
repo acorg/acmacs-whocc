0a3b0c0b40049f547e49359ca774a0a99824a115
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-10 19:20:57 +0800

    Delete py directory

--- py/acmacs_whocc/__init__.py
+++
@@ -1,6 +0,0 @@
-from .get_recent_merges import get_recent_merges
-
-# ======================================================================
-### Local Variables:
-### eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
-### End:
