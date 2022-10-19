f7029f7132bd7f9d22388358ad3a1fabafa211f7
Eugene Skepner <github@skepner.eu>
2022-10-19 10:04:53 +0200

    chains-202105 scripts

--- web/chains-202105/bin/stop-update-start.sh
+++ web/chains-202105/bin/stop-update-start.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -o errexit -o errtrace -o pipefail -o nounset
 cd $(dirname $0)/..
-./bin/stop.sh
+./bin/stop.sh || true
 ./bin/update.sh >>./gunicorn/log/update.log 2>&1
 ./bin/start.sh
