fd698d6612a190ba4d63af4a8cb34784951412eb
Eugene Skepner <github@skepner.eu>
2022-10-22 11:32:55 +0200

    bug fix

--- web/chains-202105/bin/stop-update-start.sh
+++ web/chains-202105/bin/stop-update-start.sh
@@ -2,6 +2,8 @@
 set -o errexit -o errtrace -o pipefail -o nounset
 cd $(dirname $0)/..
 export ACMACSD_ROOT=${ACMACSD_ROOT=/syn/eu/AD}
+export LOCATIONDB_V2=${LOCATIONDB_V2=/syn/eu/acmacs-data/locationdb.json.xz}
+export LOCDB_V2=${LOCDB_V2=$LOCATIONDB_V2}
 ./bin/stop.sh || true
 ./bin/update.sh >>./gunicorn/log/update.log 2>&1
 ./bin/start.sh
