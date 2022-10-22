ef44da0d6a63803f58ffa6cbd4519adb5230fb5d
Eugene Skepner <github@skepner.eu>
2022-10-22 11:24:32 +0200

    bug fix

--- web/chains-202105/bin/stop-update-start.sh
+++ web/chains-202105/bin/stop-update-start.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 set -o errexit -o errtrace -o pipefail -o nounset
 cd $(dirname $0)/..
+export ACMACSD_ROOT=${ACMACSD_ROOT=/syn/eu/AD}
 ./bin/stop.sh || true
 ./bin/update.sh >>./gunicorn/log/update.log 2>&1
 ./bin/start.sh
